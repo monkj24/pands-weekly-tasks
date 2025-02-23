653d48b92de3bfae20e83cbf9e7e332d9ba9b929
monkj24 <j.mnich76@gmail.com>
2025-02-23 12:29:03 +0000

    refresh

--- mywork/bank.py
+++ mywork/bank.py
@@ -1,5 +1,8 @@
 # bank.py
 # Prompt the user and read in two money amounts
+
+# This is first task , So I use chatGPI to help me with print
+# I found out that to define cents is nesessary use function with % as a reminder
 # author Joanna Mnich
 
 amount1= int(input("Enter amount1 (in cent):"))
