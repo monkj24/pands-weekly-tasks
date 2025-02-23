bf347817212a39aa510dbfc2c683115cd7da18bd
monkj24 <j.mnich76@gmail.com>
2025-02-23 10:55:17 +0000

    newtask

---
+++ mywork/accounts.py
@@ -0,0 +1,11 @@
+# Reads in a 10 characters account number and outputs the account number with last 4 numbers showing
+# the first 6 characters replaced with Xs
+
+# First I thought that if function [0:4] print first 4 digits, so I can use function with [6:0] and print me last 4
+# I received no answer because in python (now, I know), first index must be lower.
+# 
+
+
+# Author : joanna Mnich
+
+name = input("Please enter a 10 digit account number: ")
