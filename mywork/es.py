131f0a13febe8a9407e5356cf2f25837f7d5d1b1
monkj24 <j.mnich76@gmail.com>
2025-03-19 10:39:14 +0000

    moby

---
+++ mywork/es.py
@@ -0,0 +1,26 @@
+# A program reads in a text file and outputs the number of e's it contains. 
+
+
+# Author: Joanna Mnich
+ 
+import json
+def some_function(args: list):
+        print(args)
+
+args = ["moby-dick.txt"]
+some_function(args)
+
+filename = input("Moby-Dick ")
+
+import os
+if os.path.exists(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read()
+        print(content)
+else:
+    print(f"The file {filename} does not exist.")
+
+with open(filename, 'r', encoding='utf-8') as file:
+    content = file.read().lower()
+    e_count = content.count('e')
+print(f"The file '{filename}' contains {e_count} 'e's.")
