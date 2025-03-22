cce4d124bd68962da291ff9e9ba230f8b7c7e970
monkj24 <j.mnich76@gmail.com>
2025-03-22 17:29:32 +0000

    update

--- mywork/weekday.py
+++ mywork/weekday.py
@@ -1,16 +1,27 @@
 # Write a program that outputs whether or not today is a weekday.
-# First I checked what function today show me : day name, date or number
+# First I checked what function today show me : day name, date ,or number
 # then I use it as a number
+# and add days name
+
 #Author: Joanna Mnich
 
-from datetime import datetime
+from datetime import datetime  #https://www.geeksforgeeks.org/python-program-to-find-day-of-the-week-for-a-given-date/
 
 today = datetime.today().weekday()
 
-print(today)
+days = {
+    0: "Monday",
+    1: "Tuesday",  
+    2: "Wednesday",
+    3: "Thursday",
+    4: "Friday",
+    5: "Saturday",  
+    6: "Sunday"
+}
 
-if today > 4:
-    print ('It is the weekend, yay!')
+print(f"Today is {days[today]}")
+
+if today in [5, 6]:
+    print("It is the weekend, yay!")
 else:
-    print ('Yes, unfortunately today is a weekday')
-
+    print("Yes, unfortunately today is a weekday.")
