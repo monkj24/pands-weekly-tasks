d2d91c363217334f6405f1f74700c5684469074d
monkj24 <j.mnich76@gmail.com>
2025-04-13 19:46:12 +0000

    new

--- mywork/plottask.py
+++ mywork/plottask.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # PLOTTASK
 # A histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2, 
 # A plot of the function  h(x)=x3 in the range 0 to 10, 
@@ -18,11 +19,18 @@ import numpy as np   # numpy used for numerical operations, here to generate ran
 import matplotlib.pyplot as plt    # matplotlib import librery to creating plots, 
                                    # pyplot create the figure, axes 
 
+=======
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+>>>>>>> 66af136 (new)
 # Parameters for the normal distribution
 mean = 5
 std_dev = 2
 num_samples = 1000
 
+<<<<<<< HEAD
 np.random.seed(1) # seed added to ensure data not change after every time when run the code
 normData = np.random.normal(mean, std_dev, num_samples) # result of random stored in normData
 
@@ -63,3 +71,30 @@ plt.show()
 
 
 
+=======
+# Generate random values from a normal distribution
+normal_data = np.random.normal(mean, std_dev, num_samples)
+
+# Generate x values for the cubic function
+x_values = np.linspace(0, 10, 100)
+h_values = x_values**3
+
+# Create a figure and axis
+plt.figure(figsize=(10, 6))
+
+# Plot the histogram of the normal distribution
+plt.hist(normal_data, bins=30, density=True, alpha=0.5, color='blue', label='Normal Distribution (µ=5, σ=2)')
+
+# Plot the cubic function
+plt.plot(x_values, h_values, color='red', label='h(x) = x^3', linewidth=2)
+
+# Add labels and title
+plt.title('Histogram of Normal Distribution and Function h(x) = x^3')
+plt.xlabel('x')
+plt.ylabel('Density / h(x)')
+plt.legend()
+plt.grid()
+
+# Show the plot
+plt.show()
+>>>>>>> 66af136 (new)
