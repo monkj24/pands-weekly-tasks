162537ac6171dd4916e5cd99aa504f235d41b28a
monkj24 <j.mnich76@gmail.com>
2025-04-27 11:12:22 +0100

    Update plottask.py

--- mywork/plottask.py
+++ mywork/plottask.py
@@ -1,6 +1,6 @@
 <<<<<<< HEAD
 # PLOTTASK
-# A histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2, 
+# A histogram of a normal distribution of 1000 values with a mean of 5 and a standard deviation of 2, 
 # A plot of the function  h(x)=x3 in the range 0 to 10, 
 # the one set of axes.
 
@@ -31,20 +31,20 @@ std_dev = 2
 num_samples = 1000
 
 <<<<<<< HEAD
-np.random.seed(1) # seed added to ensure data not change after every time when run the code
+np.random.seed(1) # seed added to ensure data does not change whenever the code is run
 normData = np.random.normal(mean, std_dev, num_samples) # result of random stored in normData
 
 # the figure and primary axis
 fig, ax1 = plt.subplots() # creates a figure and a primary set of axes, 
-                          # subplots function returns a figure and axis object
+                          # The subplots function returns a figure and axis object
 
 
 # Histogram of the normal distribution
 
 ax1.hist(normData, bins=30, density=True, alpha=0.5, color='blue', label='Normal Distribution (µ=5, σ=2)')
-# the bins = 30 means the data will be divided into 30 bins
-# density is a probality density, which is the area under the histogram.
-# alpha - 0.5 makes the bars semi transparent
+# The bins = 30 means the data will be divided into 30 bins
+# Density is a probability density, which is the area under the histogram.
+# alpha - 0.5 makes the bars semi-transparent
 # color of bars
 ax1.set_ylabel('Density', color='blue')
 ax1.tick_params(axis='y', labelcolor='blue')
@@ -54,7 +54,7 @@ ax2 = ax1.twinx()
 x = np.linspace(0, 10, 100)
 h_x = x**3
 
-# An axis for h(x) to show histogram and plot on the one set of axes
+# An axis for h(x) to show the histogram and plot on the one set of axes
 ax2.plot(x, h_x, color='red', linestyle='--', linewidth=2, label='h(x) = x³') 
 ax2.set_ylabel('h(x)', color='red')
 ax2.tick_params(axis='y', labelcolor='red') # Params = parameters
@@ -79,7 +79,7 @@ normal_data = np.random.normal(mean, std_dev, num_samples)
 x_values = np.linspace(0, 10, 100)
 h_values = x_values**3
 
-# Create a figure and axis
+# Create a figure and an axis
 plt.figure(figsize=(10, 6))
 
 # Plot the histogram of the normal distribution
